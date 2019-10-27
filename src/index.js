c3d0ed3d54c14fc0926a5305e96b45ac4b19d347
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-27 10:04:09 +0900

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
-import { setUser } from './actions'
+import { setUser, clearUser } from './actions'
 
 const store = createStore(rootReducer, composeWithDevTools())
 
@@ -25,6 +25,9 @@ class Root extends Component {
       if (user) {
         this.props.setUser(user)
         this.props.history.push('/')
+      } else {
+        this.props.history.push('/login')
+        this.props.clearUser()
       }
     })
   }
@@ -48,7 +51,7 @@ const mapStateFromProps = state => ({
 const RootWithAuth = withRouter(
   connect(
     mapStateFromProps,
-    { setUser }
+    { setUser, clearUser }
   )(Root)
 )
 
